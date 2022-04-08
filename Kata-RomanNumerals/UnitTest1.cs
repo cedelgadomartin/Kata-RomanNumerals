59d984aa293c1b99f44d8296091cd64b18afee5f
Cristina Delgado <cristina.delgado@robotics.es>
2022-04-08 11:29:53 +0200

    Add project files.

---
+++ Kata-RomanNumerals/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Kata_RomanNumerals
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
